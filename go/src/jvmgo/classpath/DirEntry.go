c2f80927895f7e56ba3ac15e6f0d24519594795d
罗旭维 <luoxuwei@luoxuweideMacBook-Pro.local>
2019-10-08 22:20:44 +0800

    实现dir entry 的 entry 接口方法

--- go/src/jvmgo/classpath/DirEntry.go
+++ go/src/jvmgo/classpath/DirEntry.go
@@ -1,15 +1,31 @@
 package classpath
 
-import "path/filepath"
+import (
+	"io/ioutil"
+	"path/filepath"
+)
 
 type DirEntry struct {
 	absDir string
 }
 
-func (self *DirEntry) newDirEntry(path string)  *DirEntry {
+func newDirEntry(path string)  *DirEntry {
     absDir, err := filepath.Abs(path)
     if err != nil {
     	panic(err)
 	}
 	return &DirEntry{absDir}
+}
+
+func (self *DirEntry) readClass(name string) ([]byte, Entry, error)  {
+    fileName := filepath.Join(self.absDir, name)
+    data, err := ioutil.ReadFile(fileName)
+    if err != nil {
+    	return nil, self, err
+	}
+    return data, self, nil
+}
+
+func (self *DirEntry) String() string  {
+	return self.absDir
 }

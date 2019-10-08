b14359d385e9700c0a96930e37c805eab59226d9
luoxuwei <xuwei.luo@nio.com>
2019-10-08 18:02:42 +0800

    定义Entry接口

---
+++ go/src/jvmgo/classpath/Entry.go
@@ -0,0 +1,11 @@
+package classpath
+
+import "os"
+import "strings"
+
+const pathListSeparator = string(os.PathListSeparator)
+
+type Entry interface {
+	readClass(className string) ([]byte, Entry, error)
+	String() string
+}

ef93cbe394b08ae7e3301dc45f57fb0d7accaa43
罗旭维 <luoxuwei@luoxuweideMacBook-Pro.local>
2019-10-10 22:31:57 +0800

    创建描述class结构的结构体，实现读取魔数的方法

--- go/src/jvmgo/classfile/ClassFile.go
+++ go/src/jvmgo/classfile/ClassFile.go
@@ -1,1 +1,14 @@
 package classfile
+
+type ClassFile struct {
+	magic          uint32
+	MinorVersion   uint32
+	MajorVersion   uint32
+}
+
+func (self *ClassFile) readAndCheckMagic(reader *ClassReader) {
+    self.magic = reader.readUint32()
+    if self.magic != 0xCAFEBABE {
+    	panic("java.lang.ClassFormatError: magic!")
+	}
+}

ae430783fd92e056677f1e50fe2837c3d29d7d7c
罗旭维 <luoxuwei@luoxuweideMacBook-Pro.local>
2019-10-10 23:35:39 +0800

    实现其他数值类型常量解析

--- go/src/jvmgo/classfile/CpNumeric.go
+++ go/src/jvmgo/classfile/CpNumeric.go
@@ -1,5 +1,14 @@
 package classfile
 
+import "math"
+
+/*
+CONSTANT_Integer_info {
+    u1 tag;
+    u4 bytes;
+}
+*/
+
 type ConstantIntegerInfo struct {
 	val int32
 }
@@ -7,3 +16,52 @@ type ConstantIntegerInfo struct {
 func (self *ConstantIntegerInfo) readInfo(reader *ClassReader) {
 	self.val = int32(reader.readUint32())
 }
+
+/*
+CONSTANT_Float_info {
+    u1 tag;
+    u4 bytes;
+}
+*/
+
+type ConstantFloatInfo struct {
+	val float32
+}
+
+func (self *ConstantFloatInfo) readInfo(reader *ClassReader) {
+	self.val = math.Float32frombits(reader.readUint32())
+}
+
+/*
+CONSTANT_Long_info {
+    u1 tag;
+    u4 high_bytes;
+    u4 low_bytes;
+}
+*/
+
+type ConstantLongInfo struct {
+	val int64
+}
+
+func (self *ConstantLongInfo) readInfo(reader *ClassReader) {
+	self.val = int64(reader.readUint64())
+}
+
+/*
+CONSTANT_Double_info {
+    u1 tag;
+    u4 high_bytes;
+    u4 low_bytes;
+}
+*/
+
+type ConstantDoubleInfo struct {
+	val float64
+}
+
+func (self *ConstantDoubleInfo) readInfo(reader *ClassReader) {
+	self.val = math.Float64frombits(reader.readUint64())
+}
+
+

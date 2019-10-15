4246c01e314a282a00a8ab896f40e94f70119b3c
罗旭维 <luoxuwei@luoxuweideMacBook-Pro.local>
2019-10-15 22:00:05 +0800

    实现线程的操作栈的几个基本方法

--- go/src/jvmgo/rtda/Stack.go
+++ go/src/jvmgo/rtda/Stack.go
@@ -1,5 +1,21 @@
 package rtda
 
 type Stack struct {
+   _top    *Frame
+}
 
+func newStack(size int) *Stack {
+	return &Stack{}
+}
+
+func (self *Stack) push(frame *Frame) {
+
+}
+
+func (self *Stack) pop() *Frame {
+	return self._top
+}
+
+func (self *Stack) top() *Frame {
+	return self._top
 }
--- go/src/jvmgo/rtda/Thread.go
+++ go/src/jvmgo/rtda/Thread.go
@@ -4,3 +4,19 @@ type Thread struct {
    pc int
    stack *Stack
 }
+
+func newThread() *Thread {
+	return &Thread{stack:newStack(1024)}
+}
+
+func (self *Thread) PushFrame(frame *Frame) {
+    self.stack.push(frame)
+}
+
+func (self *Thread) PopFrame() *Frame {
+	return self.stack.pop()
+}
+
+func (self *Thread) CurrentFrame() *Frame {
+	return self.stack.top()
+}

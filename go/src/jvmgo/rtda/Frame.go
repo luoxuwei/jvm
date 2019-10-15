3a0a51f36a6820e010015c8e9be1347308dffe4c
罗旭维 <luoxuwei@luoxuweideMacBook-Pro.local>
2019-10-15 23:04:29 +0800

    完善帧实现

--- go/src/jvmgo/rtda/Frame.go
+++ go/src/jvmgo/rtda/Frame.go
@@ -2,4 +2,13 @@ package rtda
 
 type Frame struct {
     lower    *Frame
+    localVars LocalVars
+    operandStack *OperandStack
+}
+
+func newFrame(maxLocals, maxStack uint) *Frame {
+    return &Frame{
+        localVars:newLocalVars(maxLocals),
+        operandStack:newOperandStack(maxStack),
+    }
 }

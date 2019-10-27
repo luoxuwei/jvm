615ea3ced45444e1349f8888b94be0d47b46c808
罗旭维 <luoxuwei@luoxuweideMacBook-Pro.local>
2019-10-27 22:50:53 +0800

    dstore系列指令，索引在操作码

--- go/src/jvmgo/instructions/stores/dstore.go
+++ go/src/jvmgo/instructions/stores/dstore.go
@@ -16,3 +16,27 @@ type DSTORE struct {
 func (self *DSTORE) Execute(frame *rtda.Frame) {
 	_dstore(frame, self.Index)
 }
+
+type DSTORE_0 struct{ base.NoOperandsInstruction }
+
+func (self *DSTORE_0) Execute(frame *rtda.Frame) {
+	_dstore(frame, 0)
+}
+
+type DSTORE_1 struct{ base.NoOperandsInstruction }
+
+func (self *DSTORE_1) Execute(frame *rtda.Frame) {
+	_dstore(frame, 1)
+}
+
+type DSTORE_2 struct{ base.NoOperandsInstruction }
+
+func (self *DSTORE_2) Execute(frame *rtda.Frame) {
+	_dstore(frame, 2)
+}
+
+type DSTORE_3 struct{ base.NoOperandsInstruction }
+
+func (self *DSTORE_3) Execute(frame *rtda.Frame) {
+	_dstore(frame, 3)
+}

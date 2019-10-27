d040504161e1f46d3dc8d066529982d799a6883a
罗旭维 <luoxuwei@luoxuweideMacBook-Pro.local>
2019-10-27 22:21:28 +0800

    dload系列指令，索引在操作码

--- go/src/jvmgo/instructions/loads/dload.go
+++ go/src/jvmgo/instructions/loads/dload.go
@@ -16,3 +16,29 @@ type DLOAD struct {
 func (self *DLOAD) Execute(frame *rtda.Frame) {
 	_dload(frame, self.Index)
 }
+
+type DLOAD_0 struct {
+	base.NoOperandsInstruction
+}
+
+func (self *DLOAD_0) Execute(frame *rtda.Frame) {
+	_dload(frame, 0)
+}
+
+type DLOAD_1 struct{ base.NoOperandsInstruction }
+
+func (self *DLOAD_1) Execute(frame *rtda.Frame) {
+	_dload(frame, 1)
+}
+
+type DLOAD_2 struct{ base.NoOperandsInstruction }
+
+func (self *DLOAD_2) Execute(frame *rtda.Frame) {
+	_dload(frame, 2)
+}
+
+type DLOAD_3 struct{ base.NoOperandsInstruction }
+
+func (self *DLOAD_3) Execute(frame *rtda.Frame) {
+	_dload(frame, 3)
+}

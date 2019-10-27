1746644038ff526e7cf094e42e47f2deb074432a
罗旭维 <luoxuwei@luoxuweideMacBook-Pro.local>
2019-10-27 22:16:30 +0800

    aload系列指令，索引在操作码

--- go/src/jvmgo/instructions/loads/aload.go
+++ go/src/jvmgo/instructions/loads/aload.go
@@ -16,3 +16,29 @@ type ALOAD struct {
 func (self *ALOAD) Execute(frame *rtda.Frame) {
 	_aload(frame, self.Index)
 }
+
+type ALOAD_0 struct {
+	base.NoOperandsInstruction
+}
+
+func (self *ALOAD_0) Execute(frame *rtda.Frame) {
+	_aload(frame, 0)
+}
+
+type ALOAD_1 struct{ base.NoOperandsInstruction }
+
+func (self *ALOAD_1) Execute(frame *rtda.Frame) {
+	_aload(frame, 1)
+}
+
+type ALOAD_2 struct{ base.NoOperandsInstruction }
+
+func (self *ALOAD_2) Execute(frame *rtda.Frame) {
+	_aload(frame, 2)
+}
+
+type ALOAD_3 struct{ base.NoOperandsInstruction }
+
+func (self *ALOAD_3) Execute(frame *rtda.Frame) {
+	_aload(frame, 3)
+}

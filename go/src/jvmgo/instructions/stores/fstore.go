62d41ea8f52319d2d0b72db982e44e15ba38d7e2
罗旭维 <luoxuwei@luoxuweideMacBook-Pro.local>
2019-10-27 22:55:06 +0800

    fstore系列指令，索引在操作码

--- go/src/jvmgo/instructions/stores/fstore.go
+++ go/src/jvmgo/instructions/stores/fstore.go
@@ -16,3 +16,27 @@ type FSTORE struct {
 func (self *FSTORE) Execute(frame *rtda.Frame) {
 	_fstore(frame, self.Index)
 }
+
+type FSTORE_0 struct{ base.NoOperandsInstruction }
+
+func (self *FSTORE_0) Execute(frame *rtda.Frame) {
+	_fstore(frame, 0)
+}
+
+type FSTORE_1 struct{ base.NoOperandsInstruction }
+
+func (self *FSTORE_1) Execute(frame *rtda.Frame) {
+	_fstore(frame, 1)
+}
+
+type FSTORE_2 struct{ base.NoOperandsInstruction }
+
+func (self *FSTORE_2) Execute(frame *rtda.Frame) {
+	_fstore(frame, 2)
+}
+
+type FSTORE_3 struct{ base.NoOperandsInstruction }
+
+func (self *FSTORE_3) Execute(frame *rtda.Frame) {
+	_fstore(frame, 3)
+}

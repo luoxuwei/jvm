937ba9ec537955dd0c39852c9ca5a07265bb50cf
罗旭维 <luoxuwei@luoxuweideMacBook-Pro.local>
2019-10-27 22:24:22 +0800

    fload系列指令，索引在操作码

--- go/src/jvmgo/instructions/loads/fload.go
+++ go/src/jvmgo/instructions/loads/fload.go
@@ -17,3 +17,27 @@ type FLOAD struct {
 func (self *FLOAD) Execute(frame *rtda.Frame) {
 	_fload(frame, self.Index)
 }
+
+type FLOAD_0 struct{ base.NoOperandsInstruction }
+
+func (self *FLOAD_0) Execute(frame *rtda.Frame) {
+	_fload(frame, 0)
+}
+
+type FLOAD_1 struct{ base.NoOperandsInstruction }
+
+func (self *FLOAD_1) Execute(frame *rtda.Frame) {
+	_fload(frame, 1)
+}
+
+type FLOAD_2 struct{ base.NoOperandsInstruction }
+
+func (self *FLOAD_2) Execute(frame *rtda.Frame) {
+	_fload(frame, 2)
+}
+
+type FLOAD_3 struct{ base.NoOperandsInstruction }
+
+func (self *FLOAD_3) Execute(frame *rtda.Frame) {
+	_fload(frame, 3)
+}

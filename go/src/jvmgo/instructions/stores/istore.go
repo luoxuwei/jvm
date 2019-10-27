ac166dda885813db86b8b659a23bd653dba838b7
罗旭维 <luoxuwei@luoxuweideMacBook-Pro.local>
2019-10-27 22:45:23 +0800

    istore系列指令，索引在操作码

--- go/src/jvmgo/instructions/stores/istore.go
+++ go/src/jvmgo/instructions/stores/istore.go
@@ -15,3 +15,27 @@ type ISTORE struct {
 func (self *ISTORE) Execute(frame *rtda.Frame) {
 	_istore(frame, self.Index)
 }
+
+type ISTORE_0 struct{ base.NoOperandsInstruction }
+
+func (self *ISTORE_0) Execute(frame *rtda.Frame) {
+	_istore(frame, 0)
+}
+
+type ISTORE_1 struct{ base.NoOperandsInstruction }
+
+func (self *ISTORE_1) Execute(frame *rtda.Frame) {
+	_istore(frame, 1)
+}
+
+type ISTORE_2 struct{ base.NoOperandsInstruction }
+
+func (self *ISTORE_2) Execute(frame *rtda.Frame) {
+	_istore(frame, 2)
+}
+
+type ISTORE_3 struct{ base.NoOperandsInstruction }
+
+func (self *ISTORE_3) Execute(frame *rtda.Frame) {
+	_istore(frame, 3)
+}

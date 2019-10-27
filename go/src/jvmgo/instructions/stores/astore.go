de0a46c64b9ea66cd105a0266c5ab7edbb632e50
罗旭维 <luoxuwei@luoxuweideMacBook-Pro.local>
2019-10-27 22:41:14 +0800

    astore系列指令，索引在操作码

--- go/src/jvmgo/instructions/stores/astore.go
+++ go/src/jvmgo/instructions/stores/astore.go
@@ -16,3 +16,27 @@ type ASTORE struct {
 func (self *ASTORE) Execute(frame *rtda.Frame) {
 	_astore(frame, self.Index)
 }
+
+type ASTORE_0 struct{ base.NoOperandsInstruction }
+
+func (self *ASTORE_0) Execute(frame *rtda.Frame) {
+	_astore(frame, 0)
+}
+
+type ASTORE_1 struct{ base.NoOperandsInstruction }
+
+func (self *ASTORE_1) Execute(frame *rtda.Frame) {
+	_astore(frame, 1)
+}
+
+type ASTORE_2 struct{ base.NoOperandsInstruction }
+
+func (self *ASTORE_2) Execute(frame *rtda.Frame) {
+	_astore(frame, 2)
+}
+
+type ASTORE_3 struct{ base.NoOperandsInstruction }
+
+func (self *ASTORE_3) Execute(frame *rtda.Frame) {
+	_astore(frame, 3)
+}

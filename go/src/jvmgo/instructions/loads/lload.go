a82664795d6723c914cb146ecf8e1fd958e640d4
罗旭维 <luoxuwei@luoxuweideMacBook-Pro.local>
2019-10-27 22:28:53 +0800

    lload系列指令，索引在操作码

--- go/src/jvmgo/instructions/loads/lload.go
+++ go/src/jvmgo/instructions/loads/lload.go
@@ -16,3 +16,29 @@ type LLOAD struct {
 func (self *LLOAD) Execute(frame *rtda.Frame) {
 	_lload(frame, self.Index)
 }
+
+type LLOAD_0 struct {
+	base.NoOperandsInstruction
+}
+
+func (self *LLOAD_0) Execute(frame *rtda.Frame) {
+	_lload(frame, 0)
+}
+
+type LLOAD_1 struct{ base.NoOperandsInstruction }
+
+func (self *LLOAD_1) Execute(frame *rtda.Frame) {
+	_lload(frame, 1)
+}
+
+type LLOAD_2 struct{ base.NoOperandsInstruction }
+
+func (self *LLOAD_2) Execute(frame *rtda.Frame) {
+	_lload(frame, 2)
+}
+
+type LLOAD_3 struct{ base.NoOperandsInstruction }
+
+func (self *LLOAD_3) Execute(frame *rtda.Frame) {
+	_lload(frame, 3)
+}
